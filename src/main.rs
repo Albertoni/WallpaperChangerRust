b0e28c89ea8c65fa7712b5a6f587504814ec7ace
João Vicente Albertoni <joao_albertoni@hotmail.com>
2019-10-29 19:08:34 -0300

    Prototype finished

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
 extern crate wallpaper;
+extern crate systray;
+
+use std::{thread, time::Duration};
 
 fn main() {
-    println!("{:?}", wallpaper::get());
-    wallpaper::set_from_url("https://source.unsplash.com/random").unwrap();
-    println!("{:?}", wallpaper::get());
+    register_tray_icon();
+
+    loop {
+        // TODO: https://doc.rust-lang.org/std/thread/fn.sleep.html
+        // On Unix platforms, the underlying syscall may be interrupted by a spurious wakeup or signal handler.
+        // To ensure the sleep occurs for at least the specified duration, this function may invoke that system call multiple times.\
+
+        wallpaper::set_from_url("https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ssa/GEOCOLOR/3600x2160.jpg").unwrap();
+        thread::sleep(Duration::from_secs(60 * 20));
+    }
+}
+
+fn register_tray_icon() {
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create window!")
+    }
+
+    app.set_icon_from_file(&".\\icon.ico".to_string()).ok();
+
+    app.add_menu_item(&"Quit".to_string(), |window| {
+        window.quit();
+        ::std::process::exit(0);
+    }).ok();
+
+    println!("Waiting on message!");
+    app.wait_for_message();
 }
