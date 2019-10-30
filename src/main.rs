d64705147087d736ac8c3b36b6195551c22086da
João Vicente Albertoni <joao_albertoni@hotmail.com>
2019-10-29 23:35:07 -0300

    Bugfix: Doesn't freezes anymore when entering the message loop

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ extern crate systray;
 use std::{thread, time::Duration};
 
 fn main() {
+    thread::spawn(|| {
+        loop {
+            // TODO: https://doc.rust-lang.org/std/thread/fn.sleep.html
+            // On Unix platforms, the underlying syscall may be interrupted by a spurious wakeup or signal handler.
+            // To ensure the sleep occurs for at least the specified duration, this function may invoke that system call multiple times.\
+
+            println!("HENLO TIMER");
+            wallpaper::set_from_url("https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ssa/GEOCOLOR/3600x2160.jpg").unwrap();
+            thread::sleep(Duration::from_secs(1200));
+        }
+    });
+
     register_tray_icon();
-
-    loop {
-        // TODO: https://doc.rust-lang.org/std/thread/fn.sleep.html
-        // On Unix platforms, the underlying syscall may be interrupted by a spurious wakeup or signal handler.
-        // To ensure the sleep occurs for at least the specified duration, this function may invoke that system call multiple times.\
-
-        wallpaper::set_from_url("https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ssa/GEOCOLOR/3600x2160.jpg").unwrap();
-        thread::sleep(Duration::from_secs(60 * 20));
-    }
 }
 
 fn register_tray_icon() {
@@ -35,7 +38,7 @@ fn register_tray_icon() {
 
     app.add_menu_item(&"Quit".to_string(), |window| {
         window.quit();
-        ::std::process::exit(0);
+        std::process::exit(0);
     }).ok();
 
     println!("Waiting on message!");
